d3d08f5a62445716a8a73d52ffc92af4ea7b9a15
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-05-22 11:49:31 +0530

    flutter_lints added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http_response_handling/comedy_screen.dart';
 import 'package:http_response_handling/models/categories_model.dart';
